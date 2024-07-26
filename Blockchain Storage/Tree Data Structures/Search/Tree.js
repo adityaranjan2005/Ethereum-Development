693c27e91035df1ac857f12269123aa77b482f56
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-07-26 09:49:49 +0530

    Create Tree.js

---
+++ Blockchain Storage/Tree Data Structures/Search/Tree.js
@@ -0,0 +1,53 @@
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    hasNode(data) {
+        return this.searchRoot(this.root, data)
+    }
+
+    searchRoot(root, data) {
+        if(!root) {
+            return false;
+        }
+        if(root.data === data) {
+            return true;
+        }
+        if(root.data > data) {
+            return this.searchRoot(root.left, data);
+        }
+        if(root.data < data) {
+            return this.searchRoot(root.right, data);
+        }
+    }
+
+    addNode(node) {
+        if(!this.root) {
+            this.root = node;
+        }
+        this.addToRoot(this.root, node);
+    }
+
+    addToRoot(root, node) {
+        if (node.data < root.data) {
+            if (!root.left) {
+                root.left = node;
+            }
+            else {
+                this.addToRoot(root.left, node);
+            }
+        }
+
+        if (node.data > root.data) {
+            if (!root.right) {
+                root.right = node;
+            }
+            else {
+                this.addToRoot(root.right, node);
+            }
+        }
+    }
+}
+
+module.exports = Tree;
